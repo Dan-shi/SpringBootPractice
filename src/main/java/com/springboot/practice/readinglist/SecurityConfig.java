e741f63cd0287bd2bcf95c8ee2d9a18bd301e1b4
dan <1055430512@qq.com>
2019-06-10 16:51:22 +0800

    "little change"

--- src/main/java/com/springboot/practice/readinglist/SecurityConfig.java
+++ src/main/java/com/springboot/practice/readinglist/SecurityConfig.java
@@ -19,22 +19,17 @@ import org.springframework.security.core.userdetails.
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private ReaderRepository readerRepository;
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeRequests()
-//                .antMatchers("/").access("hasRole('READER')")
-//                .antMatchers("/**").permitAll()
-//                .and()
-//                .formLogin()
-//                .loginPage("/login")
-//                .failureUrl("/login?error=true");
-//    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/").access("hasRole('READER')")
+                .antMatchers("/**").permitAll();
+    }
     @Override
     protected void configure(
             AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .userDetailsService(new UserDetailsService() {
+        auth.userDetailsService(new UserDetailsService() {
                     @Override
                     public UserDetails loadUserByUsername(String username)
                             throws UsernameNotFoundException {
--- src/test/java/com/springboot/practice/readinglist/ReadinglistApplicationTests.java
+++ src/test/java/com/springboot/practice/readinglist/ReadinglistApplicationTests.java
@@ -5,12 +5,24 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ReadinglistApplicationTests {
 
     @Test
     public void contextLoads() {
+        Map<String, String> content = new HashMap<>();
+        content.put("11", "22");
+        Map<String, String> test = Collections.unmodifiableMap(content);
+        System.out.println(content.get("11"));
+
+        content.put("11", "55");
+        System.out.println(content.get("11"));
     }
 
 }
