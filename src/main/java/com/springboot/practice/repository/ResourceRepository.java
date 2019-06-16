638e76d680a46d1699cb0a6e8d17a2ba7a71bd85
FlyJeans <1055430512@qq.com>
2019-06-16 21:36:45 +0800

    add security config

--- src/main/java/com/springboot/practice/repository/ResourceRepository.java
+++ src/main/java/com/springboot/practice/repository/ResourceRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ResourceRepository extends JpaRepository<Resource, Long> {
     Resource findByResName(String resName);
+
+    Resource findByUrl(String url);
 }
